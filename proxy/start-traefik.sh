de0f42be79a7c5fe3fc1f3699434cbf6b0af00e8
Edouard <edouard.verstraete@icloud.com>
2025-10-07 00:41:33 +0200

    add logging for Let's Encrypt server selection

--- proxy/start-traefik.sh
+++ proxy/start-traefik.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "LETSENCRYPT_STAGING is set to: '$LETSENCRYPT_STAGING'"
+
 CMD="traefik \
   --providers.docker=true \
   --providers.docker.exposedbydefault=false \
@@ -10,7 +12,11 @@ CMD="traefik \
 
 # Add staging server if LETSENCRYPT_STAGING is true
 if [ "$LETSENCRYPT_STAGING" = "true" ]; then
+  echo "Using staging Let's Encrypt server"
   CMD="$CMD --certificatesresolvers.le.acme.caserver=https://acme-staging-v02.api.letsencrypt.org/directory"
+else
+  echo "Using production Let's Encrypt server"
 fi
 
+echo "Final command: $CMD"
 exec $CMD
